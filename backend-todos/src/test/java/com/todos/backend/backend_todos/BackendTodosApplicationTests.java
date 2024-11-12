bc2436b789fe101bec320306fdd7bd37ab39dc68
abraham-villalba <abraham.villalba@encora.com>
2024-11-12 12:29:16 -0700

    Initial commit, initialized the spring boot project

---
+++ backend-todos/src/test/java/com/todos/backend/backend_todos/BackendTodosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todos.backend.backend_todos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTodosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
